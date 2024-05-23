6e273ea0f9a0c200a0367ac8dfb2e0401aa7a3ce
T0nTTu <168592048+T0nTTu@users.noreply.github.com>
2024-05-23 11:08:39 +0300

    Camera and Cursor
    Added Cursor and a placeholder icon.
    Added CameraFollow and threshold point between player and cursor "CameraPosition"

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform playerMouseThresholdPoint;
+    
+    public Vector3 offset;
+    public float speed;
+    
+
+    
+
+    void Start()
+    {
+        
+    }
+   
+    void CameraPosition()
+    {
+        Vector3 desiredPos = playerMouseThresholdPoint.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPos, speed * Time.deltaTime);
+
+        Vector3 position = transform.position;
+        position.z = -10.0f;
+        transform.position = position;
+    }
+
+    
+
+
+    void Update()
+    {
+        CameraPosition();
+    }
+}
---
+++ Assets/Scripts/CursorControls/CursorController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public Texture2D cursor;
+    public Texture2D cursorClicked;
+
+    private void Awake()
+    {
+        ChangeCursor(cursor);
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    private void ChangeCursor(Texture2D cursorType)
+    {
+        // Vector2 hotspot = new Vector2(cursorType.width / 2, cursorType.height / 2);     //Changes the cursor pointer to the middle of the icon "hotspot"
+        Cursor.SetCursor(cursorType, Vector2.zero, CursorMode.Auto); //if there is a targeting icon instead of an arrow head -> change "Vector2.zero" to "hotspot"
+    }
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
