9f775c060924537c0e78abc7f72fdb23a4e386de
T0nTTu <168592048+T0nTTu@users.noreply.github.com>
2024-05-24 19:14:47 +0300

    InkShooting Bullet and explosion
    Added shooting logistics mousebutton held down
    made a placeholder "bullet" with trail renderer (prefab)
    used the placeholder ink as an explosion. (prefab)

---
+++ Assets/Scripts/Player/InkShooting.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InkShooting : MonoBehaviour
+{
+
+    public GameObject inkProjectilePrefab;
+    public GameObject inkCloudPrefab;
+    public float projectileSpeed;  
+    public float maxProjectileDistance;
+
+    private GameObject inkBullet;
+    private Vector2 shootDirection;
+    private bool isShooting = false;
+    private Vector2 playerPosition;
+
+
+    void ShootInk()
+    {
+        isShooting = true;
+
+        inkBullet = Instantiate(inkProjectilePrefab, transform.position, Quaternion.identity);
+        playerPosition = inkBullet.transform.position;
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        shootDirection = ((Vector2)(mousePosition - transform.position)).normalized;
+    }
+ 
+    void ShootingControls()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootInk();
+        }
+        if (Input.GetMouseButtonUp(0) && isShooting)
+        {
+            ExplodeInk();
+        }
+
+        if (isShooting && inkBullet != null) // if the ink exploded and there is no inkbullet. (Here is added the possible ammo count condition check) ********************** via GameManager?
+        {
+            inkBullet.transform.position +=(Vector3)(projectileSpeed * Time.deltaTime * shootDirection);
+
+            if(Vector2.Distance(playerPosition, inkBullet.transform.position) >= maxProjectileDistance)
+            {
+                ExplodeInk(); // If the ink flies too far it automatically explodes.
+            }
+        }
+    }
+
+    void ExplodeInk()
+    {
+        isShooting = false;
+        
+        Instantiate(inkCloudPrefab, inkBullet.transform.position, Quaternion.identity);
+
+        Destroy(inkBullet);
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        ShootingControls();
+    }
+}
