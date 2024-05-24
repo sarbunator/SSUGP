99dc495f379e5413b581729a1896c1c3e2cf2365
sarbunator <sarbu@live.fi>
2024-05-24 16:02:24 +0300

    Random spawn intervals
    added min & max spawn intervals to the pearl spawner

--- Assets/Scripts/Collectables/Spawner.cs
+++ Assets/Scripts/Collectables/Spawner.cs
@@ -6,12 +6,25 @@ public class Spawner : MonoBehaviour
 {
     public Transform pos;
     public GameObject[] objectsToInstantiate;
-
+    public float minSpawnInterval = 5f; // Minimum time between spawns
+    public float maxSpawnInterval = 10f; // Maximum time between spawns
 
     void Start()
     {
-        InstantiateObject();
+        StartCoroutine(SpawnObjects());
     }
+
+    IEnumerator SpawnObjects()
+    {
+
+        while (true)
+        {
+        InstantiateObject();
+            float randomInterval = Random.Range(minSpawnInterval, maxSpawnInterval);
+            yield return new WaitForSeconds(randomInterval);
+        }
+    }
+   
     
     private void InstantiateObject()
 
