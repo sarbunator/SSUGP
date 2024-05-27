c4d68842d790f65db2f4273d5523d69fdb12129b
KickingTheBucket <kickingthebucket78@outlook.com>
2024-05-27 13:21:21 +0300

    NPC patrol system
    Placeholder for enemy
    Enemy patrol system

---
+++ Assets/Scripts/NPC/EnemyPatrol.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public int targetPoint;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPoint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == patrolPoints[targetPoint].position) 
+        {
+            increaseTargetInt();
+        }
+        transform.position = Vector2.MoveTowards(transform.position,
+            patrolPoints[targetPoint].position, speed * Time.deltaTime);
+    }
+
+    void increaseTargetInt()
+    { 
+        targetPoint++;
+        if(targetPoint >= patrolPoints.Length) 
+        {
+            targetPoint = 0;
+        }
+    }
+}
