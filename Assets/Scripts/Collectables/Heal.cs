a80556e06b706cfde5eae95a276a0802e8a36bff
sarbunator <sarbu@live.fi>
2024-05-28 11:02:34 +0300

    tilemap & rigid rocks

---
+++ Assets/Scripts/Collectables/Heal.cs
@@ -0,0 +1,19 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Heal : MonoBehaviour
+//{
+//    public int healthRestore = 20;
+
+//    void Start()
+//    {
+        
+//    }
+
+//    private void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        Damageable damageable = collision.GetComponent<>
+//    }
+
+//}
