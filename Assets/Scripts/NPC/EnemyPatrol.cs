39343ed6510287d39bf013f3e1a4f99bd6ce9f6a
KickingTheBucket <kickingthebucket78@outlook.com>
2024-06-07 07:43:36 +0300

    I Broke the NPC Patrol update
    Needed to push it so we can work on it today :D

--- Assets/Scripts/NPC/EnemyPatrol.cs
+++ Assets/Scripts/NPC/EnemyPatrol.cs
@@ -8,6 +8,10 @@ public class EnemyPatrol : MonoBehaviour
     public Transform[] patrolPoints;
     public int targetPoint;
     public float speed;
+    public GameObject player;
+    public float distanceBetween;
+
+    private float distance;
  
     void Start()
     {
@@ -16,6 +20,16 @@ public class EnemyPatrol : MonoBehaviour
 
     void Update()
     {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        if (distance < distanceBetween)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed = Time.deltaTime);
+            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        }
         if (transform.position == patrolPoints[targetPoint].position) 
         {
             increaseTargetInt();
