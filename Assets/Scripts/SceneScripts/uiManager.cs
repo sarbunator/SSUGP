8db5a86f3e294182312593c2b221b9702fe6664d
sarbunator <sarbu@live.fi>
2024-06-18 15:45:06 +0300

    Menu changes
    GameUI is now disabled when any other menu/panel is open. Fixed resume button and exit button in pause menu.

--- Assets/Scripts/SceneScripts/uiManager.cs
+++ Assets/Scripts/SceneScripts/uiManager.cs
@@ -5,25 +5,40 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private GameObject gameUI; // Reference to GameUI GameObject
     [SerializeField] private GameObject gameOverPanel;
     [SerializeField] private TMP_Text restartText;
     private bool isGameOver = false;
 
     public PlayerHealth playerHealth; // reference to the PlayerHealth script (liittyy gameoverpanel delay)
+    public GameObject tutorialPanel;
+    public GameObject pauseGamePanel;
+    public GameObject optionsPanel;
 
     void Start()
     {
+        gameUI.SetActive(true); // GameUI enabloituna
         gameOverPanel.SetActive(false);
         restartText.gameObject.SetActive(false);
+
+        // muut paneelit disabloitu
+        tutorialPanel.SetActive(false);
+        pauseGamePanel.SetActive(false);
+        optionsPanel.SetActive(false);
     }
 
     void Update()
     {
+        // onko player dead ja gameover sequence ei oo alkanu viel
         if (playerHealth.isDead && !isGameOver)
         {
             isGameOver = true;
             StartCoroutine(GameOverSequence());
         }
+
+        // tarkista onko muu paneeli active, pitääkö disable GameUI
+        bool anyPanelActive = gameOverPanel.activeSelf || tutorialPanel.activeSelf || pauseGamePanel.activeSelf || optionsPanel.activeSelf;
+        gameUI.SetActive(!anyPanelActive);
 
         if (isGameOver)
         {
@@ -43,7 +58,7 @@ public class UIManager : MonoBehaviour
     private IEnumerator GameOverSequence()
     {
         yield return new WaitForSeconds(3.0f); // Delay before showing the Game Over panel
-        gameOverPanel.SetActive(true);
+        gameOverPanel.SetActive(true); 
         restartText.gameObject.SetActive(true);
     }
 }
